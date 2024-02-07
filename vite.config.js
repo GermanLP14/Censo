ec326a2d22ef90e05054fd1fb3478a75c818c582
German <germanlauritopass14@gmail.com>
2024-02-07 16:21:44 -0300

    commit de intento de publicar react app

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://germanlp14.github.io/Censo2023',
   server: {open:true, port:3000}
 })
