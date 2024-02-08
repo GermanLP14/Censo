411fc7ce6e7e6e324273a5a95fde9106840074db
GermanLP14 <56981997+GermanLP14@users.noreply.github.com>
2024-02-08 17:16:21 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,17 @@ function App() {
 
     <BrowserRouter>
       <Routes>
-        <Route path="./" element={<Registro />}></Route>
-        <Route path="./login" element={<Login />}></Route>
-        <Route path="./Home" element={<Home />}></Route>
-        <Route path="./AgregarPersona" element={<AgregarPersona />}></Route>
-        <Route path="./ListadoPersonas" element={<ListadoPersonas />}></Route>
-        <Route path="./CensadosTotales" element={<CensadosTotales />}></Route>
-        <Route path="./PersonasPorDepartamento" element={<PersonasPorDepartamento />}></Route>
-        <Route path="./PersonasPorOcupacion" element={<PersonasPorOcupacion />}></Route>
-        <Route path="./UsuariosPorDepartamento" element={<MapaUsuarios />}></Route>
-        <Route path="./PorcentajeCensados" element={<PorcentajeCensados />}></Route>
-        <Route path="./FinCenso" element={<FinCenso />}></Route>
+        <Route path="/" element={<Registro />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/Home" element={<Home />}></Route>
+        <Route path="/AgregarPersona" element={<AgregarPersona />}></Route>
+        <Route path="/ListadoPersonas" element={<ListadoPersonas />}></Route>
+        <Route path="/CensadosTotales" element={<CensadosTotales />}></Route>
+        <Route path="/PersonasPorDepartamento" element={<PersonasPorDepartamento />}></Route>
+        <Route path="/PersonasPorOcupacion" element={<PersonasPorOcupacion />}></Route>
+        <Route path="/UsuariosPorDepartamento" element={<MapaUsuarios />}></Route>
+        <Route path="/PorcentajeCensados" element={<PorcentajeCensados />}></Route>
+        <Route path="/FinCenso" element={<FinCenso />}></Route>
       </Routes>
     </BrowserRouter>
 
